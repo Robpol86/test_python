147d85a8d8f0dee58da071347809c6170dea4afd
Robpol86 <robpol86@gmail.com>
2015-06-20 11:36:40 -0700

    Create show_env.py

---
+++ show_env.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import base64
+import json
+import os
+
+ENV = os.environ.copy()
+
+
+for p in sorted(ENV.items()):
+    print('='.join(p))
+
+print(base64.b64encode(json.dumps(ENV)))
